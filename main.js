e53e6ffbf82daebe064b25f73cecef02246ec356
Juan Sebastian Lopez <104464559+juanSebastianLopez@users.noreply.github.com>
2022-08-26 17:41:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const estrellas = document.querySelector('estrellas');
+const luna = document.querySelector('luna');
+const montana = document.querySelector('montana');
+const montanasFront = document.querySelector('montanasFront');
